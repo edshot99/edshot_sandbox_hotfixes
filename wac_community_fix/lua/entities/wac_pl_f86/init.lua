aa5304e0248deba6382326e743adbfb6e3613a3f
edshot99 <ed@i2pmail.org>
2024-01-25 16:15:14 -0600

    import files from workshop - WAC Community 3 (109977794) - WAC Community 4 (128559085) - WAC Community 6 (141486806) - WAC Community 7 (162016658)

---
+++ wac_community_fix/lua/entities/wac_pl_f86/init.lua
@@ -0,0 +1,122 @@
+include("shared.lua")
+AddCSLuaFile("shared.lua")
+
+function ENT:SpawnFunction(ply, tr)
+	if (!tr.Hit) then return end
+	local ent=ents.Create(ClassName)
+	ent:SetPos(tr.HitPos+tr.HitNormal*60)
+	ent:Spawn()
+	ent:Activate()
+	ent.Owner=ply
+    ent:SetSkin(math.random(0,4))
+	ent:SetBodygroup(4,1)
+	self.Sounds=table.Copy(sndt)
+	return ent
+end
+
+ENT.AutomaticFrameAdvance = true // needed for gear anims
+
+ENT.Aerodynamics = {
+	Rotation = {
+		Front = Vector(0, -1, 0),
+		Right = Vector(0, 0, 60), -- Rotate towards flying direction
+		Top = Vector(0, 0, 0)
+	},
+	Lift = {
+		Front = Vector(0, 0, 13.25), -- Go up when flying forward
+		Right = Vector(0, 0, 0),
+		Top = Vector(0, 0, -0.25)
+	},
+	Rail = Vector(1, 5, 20),
+	Drag = {
+		Directional = Vector(0.01, 0.01, 0.01),
+		Angular = Vector(0.03, 0.05, 0.05)
+	}
+}
+
+function ENT:PhysicsUpdate(ph)
+	self:base("wac_pl_base").PhysicsUpdate(self,ph)
+
+	if not IsValid(self) or not IsValid(self:GetPhysicsObject()) then return end
+
+	if self.rotorRpm > 0.5 and self.rotorRpm < 0.89 and IsValid(self.rotorModel) then
+		self.rotorModel:SetBodygroup(1,2)
+	elseif self.rotorRpm > 0.9 and IsValid(self.rotorModel) then
+		self.rotorModel:SetBodygroup(1,2)
+	elseif self.rotorRpm < 0.8 and IsValid(self.rotorModel) then
+		self.rotorModel:SetBodygroup(1,1)
+	end
+	
+	local geardown,t1=self:LookupSequence("geardown")
+	local gearup,t2=self:LookupSequence("gearup")	
+	local trace=util.QuickTrace(self:LocalToWorld(Vector(0,0,62)), self:LocalToWorld(Vector(0,0,50)), {self, self.Wheels[1], self.Wheels[2], self.Wheels[3], self.TopRotor})
+	local phys=self:GetPhysicsObject()
+	if IsValid(phys) and not self.disabled then
+		if self.controls.throttle>0.9 and self.rotorRpm>0.8 and phys:GetVelocity():Length() > 1299 and trace.HitPos:Distance( self:LocalToWorld(Vector(0,0,62)) ) > 50  and self:GetSequence() != gearup then
+			self:ResetSequence(gearup) 
+			self:SetPlaybackRate(1.0)
+			self:SetBodygroup(2,1)
+			for i=1,3 do 
+				self.wheels[i]:SetRenderMode(RENDERMODE_TRANSALPHA)
+				self.wheels[i]:SetColor(Color(255,255,255,0))
+				self.wheels[i]:SetSolid(SOLID_NONE)
+			end
+		elseif self.controls.throttle<0.6 and trace.HitPos:Distance( self:LocalToWorld(Vector(0,0,62)) ) > 50  and self:GetSequence() == gearup then
+			self:ResetSequence(geardown)
+			self:SetPlaybackRate(1.0)
+			timer.Simple(t1,function()
+				if self.wheels then
+					for i=1,3 do
+						self.wheels[i]:SetRenderMode(RENDERMODE_NORMAL)
+						self.wheels[i]:SetColor(Color(255,255,255,255))
+						self.wheels[i]:SetSolid(SOLID_VPHYSICS)
+					end
+					self:SetBodygroup(2,0)
+				end
+			end)
+		end
+	end
+	
+	local phys=self:GetPhysicsObject()
+	if IsValid(phys) and not self.disabled then
+		if phys:GetVelocity():Length() > 850 then
+			self:SetBodygroup(2,1)
+		else
+			self:SetBodygroup(2,0)
+		end
+	end
+
+	if self.disabled and not self.backgib then
+		self:KillBackRotor()
+		self:SetBodygroup(5,1)
+		self:SetBodygroup(4,0)
+		self.backgib = ents.Create("prop_physics")
+		self.backgib:SetModel("models/chippy/f86/wing.mdl")
+		self.backgib:SetSkin(self:GetSkin())
+		self.backgib:SetPos(self:LocalToWorld(Vector(0,25,25)))
+		self.backgib:SetAngles(self:GetAngles())
+		self.backgib:Spawn()
+		self.backgib:Activate()
+		self.backgib:GetPhysicsObject():AddVelocity(self:GetVelocity()+self:GetRight()*500)
+		self.backgib:GetPhysicsObject():AddAngleVelocity(self:GetPhysicsObject():GetAngleVelocity())
+		local fire = ents.Create("env_fire_trail")
+		fire:SetPos(self.backgib:LocalToWorld(Vector(0,-50,0)))
+		fire:Spawn()
+		fire:SetParent(self.backgib)
+		self.backgib.fire=fire
+		constraint.NoCollide(self,self.backgib,0,0)
+		for k,v in pairs(self.Wheels) do
+			if IsValid(v) then
+				constraint.NoCollide(self,v,0,0)
+			end
+		end
+		self:AddOnRemove(self.backgib)
+		self:AddOnRemove(self.backgib.fire)
+	end
+	
+	if self.active then
+		self:SetBodygroup(3,0)
+	else
+		self:SetBodygroup(3,1)
+	end
+end
---
+++ wac_community_fix/lua/entities/wac_pl_f86/shared.lua
@@ -0,0 +1,181 @@
+if not wac then return end
+if SERVER then AddCSLuaFile() end
+ENT.Base 				= "wac_pl_base"
+ENT.Type 				= "anim"
+ENT.Category			= wac.aircraft.spawnCategoryC
+ENT.PrintName			= "F-86F Sabre"
+ENT.Author				= "Chippy"
+
+ENT.Spawnable			= true
+ENT.AdminSpawnable		= true
+
+ENT.Model            = "models/chippy/f86/body.mdl"
+
+ENT.EngineForce        = 480
+ENT.Weight            = 6400
+ENT.SeatSwitcherPos	= Vector(0,0,0)
+ENT.AngBrakeMul		= 0.0127
+ENT.SmokePos        = Vector(-230,1,23)
+ENT.FirePos            = Vector(50,-40,1)
+ENT.rotorPos			= Vector(0,0,0)
+
+if CLIENT then
+	ENT.thirdPerson = {
+		distance = 360
+	}
+end
+
+ENT.Wheels={
+	{
+		mdl="models/chippy/f86/bwheel1.mdl",
+		pos=Vector(-30,-47,-40),
+		friction=0,
+		mass=900,
+	},
+	{
+		mdl="models/chippy/f86/bwheel2.mdl",
+		pos=Vector(-30,47,-40),
+		friction=0,
+		mass=900,
+	},
+	{
+		mdl="models/chippy/f86/fwheel.mdl",
+		pos=Vector(136,-2,-37.8),
+		friction=0,
+		mass=700,
+	},
+}
+
+ENT.Agility = {
+	Thrust = 15
+}
+
+
+ENT.Seats = {
+	{
+		pos=Vector(60,-0.2,20.6),
+		exit=Vector(3.5,60,100),
+		weapons={"M3 Browning"},
+	}
+}
+
+ENT.Weapons = {
+	["M3 Browning"] = {
+		class = "wac_pod_gatling",
+		info = {
+			Pods = {
+				Vector(140,20,28),
+				Vector(140,20,20),
+				Vector(140,20,11),
+				Vector(140,-20,28),
+				Vector(140,-20,20),
+				Vector(140,-20,11),
+			},
+			FireRate = 500,
+			Sequential = true,
+			Sounds = {
+				shoot = "wac/f86/shoot_start.wav",
+				stop = "wac/f86/shoot_end.wav",
+			},
+		}
+	}
+}
+
+ENT.Sounds={
+	Start="wac/f86/start.wav",
+	Blades="wac/f86/external.wav",
+	Engine="wac/f86/internal.wav",
+	MissileAlert="",
+	MissileShoot="",
+	MinorAlarm="",
+	LowHealth="",
+	CrashAlarm="",
+}
+
+
+//hud
+
+local function DrawLine(v1,v2)
+	surface.DrawLine(v1.y,v1.z,v2.y,v2.z)
+end
+
+local mHorizon0 = Material("WeltEnSTurm/WAC/Helicopter/hud_line_0")
+local HudCol = Color(70,199,50,150)
+local Black = Color(0,0,0,200)
+
+local mat = {
+	Material("WeltEnSTurm/WAC/Helicopter/hud_line_0"),
+	Material("WeltEnSTurm/WAC/Helicopter/hud_line_high"),
+	Material("WeltEnSTurm/WAC/Helicopter/hud_line_low"),
+}
+
+local function getspaces(n)
+	if n<10 then
+		n="      "..n
+	elseif n<100 then
+		n="    "..n
+	elseif n<1000 then
+		n="  "..n
+	end
+	return n
+end
+
+
+function ENT:DrawPilotHud()
+	local pos = self:GetPos()
+	local ang = self:GetAngles()
+	ang:RotateAroundAxis(self:GetRight(), 90)
+	ang:RotateAroundAxis(self:GetForward(), 90)
+
+	local uptm = self.SmoothVal
+	local upm = self.SmoothUp
+	local spos=self.Seats[1].pos
+
+	cam.Start3D2D(self:LocalToWorld(Vector(30,3.75,37.75)+spos), ang,0.015)
+	surface.SetDrawColor(HudCol)
+	surface.DrawRect(234, 247, 10, 4)
+	surface.DrawRect(254, 247, 10, 4)
+	surface.DrawRect(247, 234, 4, 10)
+	surface.DrawRect(247, 254, 4, 10)
+
+	local a=self:GetAngles()
+	a.y=0
+	local up=a:Up()
+	up.x=0
+	up=up:GetNormal()
+
+	local size=180
+	local dist=10
+	local step=12
+	for p=-180,180,step do
+		if a.p+p>-size/dist and a.p+p<size/dist then
+			if p==0 then
+				surface.SetMaterial(mat[1])
+			elseif p>0 then
+				surface.SetMaterial(mat[2])
+			else
+				surface.SetMaterial(mat[3])
+			end
+			surface.DrawTexturedRectRotated(250+up.y*(a.p+p)*dist,250-up.z*(a.p+p)*dist,300,300,a.r)
+		end
+	end
+
+	surface.SetTextColor(HudCol)
+	surface.SetFont("wac_heli_small")
+
+	surface.SetTextPos(30, 410) 
+	surface.DrawText("SPD  "..math.floor(self:GetVelocity():Length()*0.1) .."kn")
+	surface.SetTextPos(30, 445)
+	local tr=util.QuickTrace(pos+self:GetUp()*10,Vector(0,0,-999999),self.Entity)
+	surface.DrawText("ALT  "..math.ceil((pos.z-tr.HitPos.z)*0.01905).."m")
+
+	if self:GetNWInt("seat_1_actwep") == 1 and self.weapons["Hydra 70"] then
+		surface.SetTextPos(300,445)
+		local n = self.weapons["Hydra 70"]:GetAmmo()
+		surface.DrawText("Hydra 70" .. getspaces(n))
+	end
+
+	cam.End3D2D()
+end
+
+function ENT:DrawWeaponSelection() end
