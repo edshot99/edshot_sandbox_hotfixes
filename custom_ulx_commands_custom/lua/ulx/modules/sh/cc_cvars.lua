9bbc4dcb968f674ce109fe5b67b2ada5fb656451
edshot99 <ed@i2pmail.org>
2024-03-08 15:58:16 -0600

    allow players to join discord group chat easily

--- custom_ulx_commands_custom/lua/ulx/modules/sh/cc_cvars.lua
+++ custom_ulx_commands_custom/lua/ulx/modules/sh/cc_cvars.lua
@@ -3,4 +3,5 @@
 ---------------
 
 CreateConVar("donate_url", "http://www.google.com");
+CreateConVar("discord_url", "http://discord.com");
 CreateConVar("explode_ragdolls", "0");
--- custom_ulx_commands_custom/lua/ulx/modules/sh/cc_menus.lua
+++ custom_ulx_commands_custom/lua/ulx/modules/sh/cc_menus.lua
@@ -9,6 +9,16 @@ local donate = ulx.command("Menus", "ulx donate", ulx.donate, "!donate");
 donate:defaultAccess(ULib.ACCESS_ALL);
 donate:help("View donation information.");
 
+function ulx.discord(calling_ply)
+	calling_ply:SendLua([[gui.OpenURL( "]] .. GetConVar("discord_url"):GetString() .. [[" )]]);
+end
+local discord = ulx.command("Menus", "ulx discord", ulx.discord, "!disc");
+discord:defaultAccess(ULib.ACCESS_ALL);
+discord:help("View Discord invite.");
+local discord2 = ulx.command("Menus", "ulx discord", ulx.discord, "!discord");
+discord2:defaultAccess(ULib.ACCESS_ALL);
+discord2:help("View Discord invite.");
+
 function ulx.soundlist(calling_ply)
 	calling_ply:ConCommand("menu_sounds");
 end
