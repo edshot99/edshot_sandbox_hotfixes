c2689f7b8eeecb9b9181e28b8788df2cad4c20d3
edshot99 <ed@i2pmail.org>
2024-03-11 12:54:37 -0500

    improvement

--- ulx_custom/lua/autorun/server/svotemap.lua
+++ ulx_custom/lua/autorun/server/svotemap.lua
@@ -1,36 +1,26 @@
 
-local votemap = GetConVar("ulx_votemapminvotes")
-local votemap2 = GetConVar("ulx_votemap2minvotes")
-
 local function svotemap()
-	local plys = player.GetHumans()
+	local votemap = GetConVar("ulx_votemapminvotes")
+	local votemap2 = GetConVar("ulx_votemap2minvotes")
 
-	if ((votemap == nil) || (votemap2 == nil)) then
-		return
+	if ((votemap ~= nil) and (votemap2 ~= nil)) then
+		local plys = player.GetHumans()
+
+		if #plys == 2 then
+			votemap:SetInt(2)
+			votemap2:SetInt(2)
+		elseif #plys <= 1 then
+			votemap:SetInt(1)
+			votemap2:SetInt(1)
+		else
+			votemap:SetInt(3)
+			votemap2:SetInt(3)
+		end
 	end
-
-	if #plys == 2 then
-		votemap:SetInt(2)
-		votemap2:SetInt(2)
-	elseif #plys <= 1 then
-		votemap:SetInt(1)
-		votemap2:SetInt(1)
-	else
-		votemap:SetInt(3)
-		votemap2:SetInt(3)
-	end
-end
-
-local function HPlayerConnect(bot, networkid, name, index, address)
-	svotemap()
-end
-
-local function HPlayerDisconnect(bot, networkid, name, userid, reason)
-	svotemap()
 end
 
 gameevent.Listen("player_connect")
 gameevent.Listen("player_disconnect")
 
-hook.Add("player_connect", "Hook_PlayerConnect", HPlayerConnect)
-hook.Add("player_disconnect", "Hook_PlayerDisconnect", HPlayerDisconnect)
+hook.Add("player_connect", "edshot_svotemap_connect", function() svotemap() end)
+hook.Add("player_disconnect", "edshot_svotemap_disconnect", function() svotemap() end)
