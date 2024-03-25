e7ada15a9c350f503eadb224ff8a3770bde33fc8
edshot99 <ed@i2pmail.org>
2024-03-25 04:28:38 -0500

    drop custom changes. moved to dedicated repository

--- custom_ulx_commands_custom/lua/ulx/modules/sh/cc_cvars.lua
+++
@@ -1,8 +0,0 @@
----------------
---  ConVars  --
----------------
-
-CreateConVar("donate_url", "http://www.google.com");
-CreateConVar("discord_url", "http://discord.com");
-CreateConVar("collection_url", "http://steampowered.com");
-CreateConVar("explode_ragdolls", "0");
